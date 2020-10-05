aea6348cf10b8c7a4736b2c190474884d5817a44
Brigido Rodriguez <brigido@image-metrics.com>
2020-10-05 10:14:12 -0700

    Formatting, and more attempts to render on iOS.

--- Application/OpenGLViewController.m
+++ Application/OpenGLViewController.m
@@ -1,3 +1,5 @@
+#define GL_SILENCE_DEPRECATION
+
 #import "OpenGLViewController.h"
 #import "BabylonManager.h"
 
@@ -17,7 +19,7 @@
 #if TARGET_IOS
 + (Class) layerClass
 {
-    return [CAEAGLLayer class];
+  return [CAEAGLLayer class];
 }
 #endif
 
@@ -25,41 +27,46 @@
 
 @implementation OpenGLViewController
 {
-    OpenGLView *_view;
-    PlatformGLContext *_context;
-    GLuint _defaultFBOName;
-    
-#if defined(TARGET_IOS) || defined(TARGET_TVOS)
-    GLuint _colorRenderbuffer;
-    CADisplayLink *_displayLink;
-#else
-    CVDisplayLinkRef _displayLink;
-#endif
+  OpenGLView *_view;
+  PlatformGLContext *_context;
   
-    BabylonManager *_babylonManager;
+  GLuint _defaultFrameBuffer;
+#if defined(TARGET_IOS) || defined(TARGET_TVOS)
+  CAEAGLLayer* _eaglLayer;
+  GLuint _colorRenderBuffer;
+  GLuint _depthRenderBuffer;
+  CADisplayLink *_displayLink;
+#else
+  CVDisplayLinkRef _displayLink;
+#endif
+  BabylonManager *_babylonManager;
 }
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    _view = (OpenGLView *)self.view;
-    [self prepareView];
-    [self makeCurrentContext];
-    _babylonManager = [[BabylonManager alloc] initWithWidth:1280 height:720];
+  [super viewDidLoad];
+  _view = (OpenGLView *)self.view;
+  [self prepareView];
+  [self makeCurrentContext];
+#if defined(TARGET_IOS) || defined(TARGET_TVOS)
+  _babylonManager = [[BabylonManager alloc] initWithWidth:750 height:1296];
+#else
+  _babylonManager = [[BabylonManager alloc] initWithWidth:1280 height:720];
+#endif
 }
 
 #if TARGET_MACOS
 
-- (CGSize) drawableSize
+- (CGSize)drawableSize
 {
-    CGSize viewSizePoints = _view.bounds.size;
-    CGSize viewSizePixels = [_view convertSizeToBacking:viewSizePoints];
-    return viewSizePixels;
+  CGSize viewSizePoints = _view.bounds.size;
+  CGSize viewSizePixels = [_view convertSizeToBacking:viewSizePoints];
+  return viewSizePixels;
 }
 
 - (void)makeCurrentContext
 {
-    [_context makeCurrentContext];
+  [_context makeCurrentContext];
 }
 
 static CVReturn OpenGLDisplayLinkCallback(CVDisplayLinkRef displayLink,
@@ -69,157 +76,206 @@ static CVReturn OpenGLDisplayLinkCallback(CVDisplayLinkRef displayLink,
                                          CVOptionFlags* flagsOut,
                                          void* displayLinkContext)
 {
-    OpenGLViewController *viewController = (__bridge OpenGLViewController*)displayLinkContext;
-    [viewController draw];
-    return YES;
+  OpenGLViewController *viewController = (__bridge OpenGLViewController*)displayLinkContext;
+  [viewController draw];
+  return YES;
 }
 
 - (void)draw
 {
-    CGLLockContext(_context.CGLContextObj);
-    [_context makeCurrentContext];
-    [_babylonManager draw];
-    CGLFlushDrawable(_context.CGLContextObj);
-    CGLUnlockContext(_context.CGLContextObj);
+  CGLLockContext(_context.CGLContextObj);
+  [_context makeCurrentContext];
+  [_babylonManager draw];
+  CGLFlushDrawable(_context.CGLContextObj);
+  CGLUnlockContext(_context.CGLContextObj);
 }
 
 - (void)prepareView
 {
-    NSOpenGLPixelFormatAttribute attrs[] =
-    {
-        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-        NSOpenGLPFAColorSize, 24,
-        NSOpenGLPFAAlphaSize, 8,
-        NSOpenGLPFADoubleBuffer,
-        NSOpenGLPFADepthSize, 24,
-        0
-    };
+  NSOpenGLPixelFormatAttribute attrs[] =
+  {
+    NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
+    NSOpenGLPFAColorSize, 24,
+    NSOpenGLPFAAlphaSize, 8,
+    NSOpenGLPFADepthSize, 24,
+    NSOpenGLPFADoubleBuffer,
+    NSOpenGLPFAAccelerated ,
+    0
+  };
   
-    NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
-    NSAssert(pixelFormat, @"No OpenGL pixel format");
-    _context = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
-    CGLLockContext(_context.CGLContextObj);
-    [_context makeCurrentContext];
-    CGLUnlockContext(_context.CGLContextObj);
+  NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+  NSAssert(pixelFormat, @"No OpenGL pixel format");
+  _context = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
+  CGLLockContext(_context.CGLContextObj);
+  [_context makeCurrentContext];
+  CGLUnlockContext(_context.CGLContextObj);
 
-    _view.pixelFormat = pixelFormat;
-    _view.openGLContext = _context;
-    _view.wantsBestResolutionOpenGLSurface = YES;
+  _view.pixelFormat = pixelFormat;
+  _view.openGLContext = _context;
+  _view.wantsBestResolutionOpenGLSurface = YES;
 
-    // Default FBO is 0 on macOS since it uses a traditional OpenGL pixel format model
-    _defaultFBOName = 0;
-    CVDisplayLinkCreateWithActiveCGDisplays(&_displayLink);
-    // Set the renderer output callback function
-    CVDisplayLinkSetOutputCallback(_displayLink, &OpenGLDisplayLinkCallback, (__bridge void*)self);
-    CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(_displayLink, _context.CGLContextObj, pixelFormat.CGLPixelFormatObj);
+  // Default FBO is 0 on macOS since it uses a traditional OpenGL pixel format model
+  _defaultFrameBuffer = 0;
+  
+  CVDisplayLinkCreateWithActiveCGDisplays(&_displayLink);
+  // Set the renderer output callback function
+  CVDisplayLinkSetOutputCallback(_displayLink, &OpenGLDisplayLinkCallback, (__bridge void*)self);
+  CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(_displayLink, _context.CGLContextObj, pixelFormat.CGLPixelFormatObj);
 }
 
 - (void)viewDidLayout
 {
-    CGLLockContext(_context.CGLContextObj);
-    NSSize viewSizePoints = _view.bounds.size;
-    NSSize viewSizePixels = [_view convertSizeToBacking:viewSizePoints];
-    [self makeCurrentContext];
-  
-    // Update size to Babylon
-    [_babylonManager setSizeWithWidth:viewSizePixels.width height:viewSizePixels.height];
-  
-    CGLUnlockContext(_context.CGLContextObj);
-    if(!CVDisplayLinkIsRunning(_displayLink))
-    {
-        CVDisplayLinkStart(_displayLink);
-    }
+  CGLLockContext(_context.CGLContextObj);
+  NSSize viewSizePoints = _view.bounds.size;
+  NSSize viewSizePixels = [_view convertSizeToBacking:viewSizePoints];
+  [self makeCurrentContext];
+
+  // Update size to Babylon
+  [_babylonManager setSizeWithWidth:viewSizePixels.width height:viewSizePixels.height];
+
+  CGLUnlockContext(_context.CGLContextObj);
+  if(!CVDisplayLinkIsRunning(_displayLink))
+  {
+    CVDisplayLinkStart(_displayLink);
+  }
 }
 
 - (void)viewWillDisappear
 {
-    CVDisplayLinkStop(_displayLink);
+  CVDisplayLinkStop(_displayLink);
 }
 
 - (void)dealloc
 {
-    CVDisplayLinkStop(_displayLink);
-    CVDisplayLinkRelease(_displayLink);
+  CVDisplayLinkStop(_displayLink);
+  CVDisplayLinkRelease(_displayLink);
 }
 
 #else
 
 - (void)draw:(id)sender
 {
-    [EAGLContext setCurrentContext:_context];
-    [_babylonManager draw];
-    
-    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderbuffer);
-    [_context presentRenderbuffer:GL_RENDERBUFFER];
+  [EAGLContext setCurrentContext:_context];
+  glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable(GL_BLEND);
+  glEnable(GL_DEPTH_TEST);
+  
+  glViewport(0, 0, 750, 1296);
+  
+  [_babylonManager draw];
+  
+  glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+  [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
 
 - (void)makeCurrentContext
 {
-    [EAGLContext setCurrentContext:_context];
+  [EAGLContext setCurrentContext:_context];
 }
 
 - (void)prepareView
 {
-    // Get the layer
-    CAEAGLLayer *eaglLayer = (CAEAGLLayer *)self.view.layer;
-    eaglLayer.drawableProperties = @{kEAGLDrawablePropertyRetainedBacking : @NO,
-                                     kEAGLDrawablePropertyColorFormat     : kEAGLColorFormatRGBA8 };
-    eaglLayer.opaque = YES;
-    _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
-    NSAssert(_context, @"Could Not Create OpenGL ES Context");
-    BOOL isSetCurrent = [EAGLContext setCurrentContext:_context];
-    NSAssert(isSetCurrent, @"Could not make OpenGL ES context current");
-    [self makeCurrentContext];
-    self.view.contentScaleFactor = [UIScreen mainScreen].nativeScale;
-    
-    // On iOS & tvOS you must create an FBO and attach a CoreAnimation allocated drawable texture
-    // to use as the "defaultFBO" for a view
-    glGenFramebuffers(1, &_defaultFBOName);
-    glBindFramebuffer(GL_FRAMEBUFFER, _defaultFBOName);
-    glGenRenderbuffers(1, &_colorRenderbuffer);
-    [self resizeDrawable];
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderbuffer);
+  [self setupLayer];
+  [self setupContext];
+//  [self makeCurrentContext];
+  self.view.contentScaleFactor = [UIScreen mainScreen].nativeScale;
+  
+  [self setupDepthBuffer];
+  [self setupRenderBuffer];
+  [self setupFrameBuffer];
+  
+  // On iOS & tvOS you must create an FBO and attach a CoreAnimation allocated drawable texture
+  // to use as the "defaultFBO" for a view
+//  glGenFramebuffers(1, &_defaultFBOName);
+//  glBindFramebuffer(GL_FRAMEBUFFER, _defaultFBOName);
+//  glGenRenderbuffers(1, &_colorRenderBuffer);
+//  [self resizeDrawable];
+//  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderBuffer);
 
-    // Create the display link so that we render at 60FPS
-    _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(draw:)];
-    _displayLink.preferredFramesPerSecond = 60;
-
-    // Have the display link run on the default runn loop (and the main thread)
-    [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+  [self setupDisplayLink];
 }
 
 - (CGSize)drawableSize
 {
-    GLint backingWidth, backingHeight;
-    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderbuffer);
-    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &backingWidth);
-    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &backingHeight);
-    CGSize drawableSize = {backingWidth, backingHeight};
-    return drawableSize;
+  GLint backingWidth, backingHeight;
+  glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+  glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &backingWidth);
+  glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &backingHeight);
+  CGSize drawableSize = {backingWidth, backingHeight};
+  return drawableSize;
+}
+
+- (void)setupDisplayLink
+{
+  // Create the display link so that we render at 60FPS
+  _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(draw:)];
+  _displayLink.preferredFramesPerSecond = 60;
+  // Have the display link run on the default runn loop (and the main thread)
+  [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
 }
 
 - (void)resizeDrawable
 {
-    [self makeCurrentContext];
+//  [self makeCurrentContext];
 
-    // Ensure we've actually got a render buffer first;
-    assert(_colorRenderbuffer != 0);
-    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderbuffer);
-    [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(id<EAGLDrawable>)_view.layer];
+  // Ensure we've actually got a render buffer first;
+  assert(_colorRenderBuffer != 0);
+  glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+  [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(id<EAGLDrawable>)_view.layer];
 
-    // Update size to Babylon
-    CGSize resolution = [self drawableSize];
-    [_babylonManager setSizeWithWidth:resolution.width height:resolution.height];
+  // Update size to Babylon
+  CGSize resolution = [self drawableSize];
+  [_babylonManager setSizeWithWidth:resolution.width height:resolution.height];
+}
+
+- (void)setupLayer
+{
+  // Get the layer
+  _eaglLayer = (CAEAGLLayer *)self.view.layer;
+  _eaglLayer.drawableProperties = @{kEAGLDrawablePropertyRetainedBacking : @NO,
+                                   kEAGLDrawablePropertyColorFormat     : kEAGLColorFormatRGBA8 };
+  _eaglLayer.opaque = YES;
+}
+
+- (void)setupContext
+{
+  _context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+  NSAssert(_context, @"Could Not Create OpenGL ES Context");
+  BOOL isSetCurrent = [EAGLContext setCurrentContext:_context];
+  NSAssert(isSetCurrent, @"Could not make OpenGL ES context current");
+}
+
+- (void)setupRenderBuffer
+{
+  glGenRenderbuffers(1, &_colorRenderBuffer);
+  glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+  [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+- (void)setupDepthBuffer
+{
+  CGSize resolution = {750, 1296};//[self drawableSize];
+  glGenRenderbuffers(1, &_depthRenderBuffer);
+  glBindRenderbuffer(GL_RENDERBUFFER, _depthRenderBuffer);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, resolution.width, resolution.height);
+}
+
+- (void)setupFrameBuffer
+{
+  glGenFramebuffers(1, &_defaultFrameBuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, _defaultFrameBuffer);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorRenderBuffer);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRenderBuffer);
 }
 
 - (void)viewDidLayoutSubviews
 {
-    [self resizeDrawable];
+  [self resizeDrawable];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [self resizeDrawable];
+  [self resizeDrawable];
 }
 
 #endif
--- Application/interface/HelloScene.cpp
+++ Application/interface/HelloScene.cpp
@@ -35,75 +35,42 @@ struct HelloScene : public BABYLON::IRenderableScene
     camera->setPosition(Vector3(0.f, 5.f, -10.f));
     camera->attachControl(canvas, true);
 
-    camera->upperBetaLimit   = Math::PI_2;
+    camera->upperBetaLimit = Math::PI_2;
     camera->lowerRadiusLimit = 4.f;
 
-    auto light       = HemisphericLight::New("light1", Vector3(0.f, 1.f, 0.f), scene);
+    auto light = HemisphericLight::New("light1", Vector3(0.f, 1.f, 0.f), scene);
     light->intensity = 0.7f;
 
     auto knot = Mesh::CreateTorusKnot("knot", 1, 0.4f, 128, 64, 2, 3, scene);
 
     // Mirror
-    auto mirror                       = Mesh::CreateBox("Mirror", 1.0, scene);
-    mirror->scaling                   = Vector3(100.f, 0.01f, 100.f);
-    auto mirrorMaterial               = StandardMaterial::New("mirror", scene);
-    auto reflectionTexture            = MirrorTexture::New("mirror", 512.f, scene, true);
-    reflectionTexture->mirrorPlane    = Plane(0.f, -1.f, 0.f, -2.f);
-    reflectionTexture->renderList     = {knot.get()};
-    reflectionTexture->level          = 1.f;
-    reflectionTexture->samples        = 8;
+    auto mirror = Mesh::CreateBox("Mirror", 1.0, scene);
+    mirror->scaling = Vector3(100.f, 0.01f, 100.f);
+    auto mirrorMaterial = StandardMaterial::New("mirror", scene);
+    auto reflectionTexture = MirrorTexture::New("mirror", 1024.f, scene, true);
+    reflectionTexture->mirrorPlane = Plane(0.f, -1.f, 0.f, -2.f);
+    reflectionTexture->renderList = {knot.get()};
+    reflectionTexture->level = 1.f;
+    reflectionTexture->samples = 16;
     mirrorMaterial->reflectionTexture = reflectionTexture;
-    mirror->material                  = mirrorMaterial;
-    mirror->position                  = Vector3(0.f, -2.f, 0.f);
+    mirror->material = mirrorMaterial;
+    mirror->position = Vector3(0.f, -2.f, 0.f);
 
     // Main material
-    auto mainMaterial            = StandardMaterial::New("main", scene);
-    knot->material               = mainMaterial;
-//    mainMaterial->diffuseTexture = Texture::New("textures/amiga.jpg", scene);
+    auto mainMaterial = StandardMaterial::New("main", scene);
+    knot->material = mainMaterial;
+    mainMaterial->diffuseColor = Color4(0.2f, 0.4f, 0.8f, 1.f);
 
     // Fog
     scene->fogMode  = Scene::FOGMODE_LINEAR;
     scene->fogColor = scene->clearColor;
     scene->fogStart = 20.f;
     scene->fogEnd   = 50.f;
-    
-//    // Create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-//    auto camera = FreeCamera::New("camera1", Vector3(0, 5, -10), scene);
-//    // Target the camera to scene origin
-//    camera->setTarget(Vector3::Zero());
-//    // Attach the camera to the canvas
-//    camera->attachControl(canvas, true);
-//    // Create a basic light, aiming 0,1,0 - meaning, to the sky
-//    auto light = HemisphericLight::New("light1", Vector3(0, 1, 0), scene);
-//    // Default intensity is 1. Let's dim the light a small amount
-//    light->intensity = 0.7f;
-//    // Create a built-in "sphere" shape; its constructor takes 5 params: name,
-//    // subdivs, size, scene
-//    auto sphere = Mesh::CreateSphere("sphere1", 16, 2.f, scene);
-//    // Move the sphere upward 1/2 of its height
-//    sphere->position().y = 1.f;
-//    // Create a built-in "ground" shape.
-//    // Params: name, width, depth, subdivs, scene
-//    Mesh::CreateGround("ground1", 6, 6, 2, scene);
-
-//    PlaneOptions options;
-//    options.width = 10.24f;
-//    options.height = 5.12f;
-//    auto plane         = MeshBuilder::CreatePlane("BackgroundPlane", options, scene);
-//    auto planeMaterial = StandardMaterial::New("ground1Material", scene);
-//    plane->material    = planeMaterial;
-//    plane->setAbsolutePosition(Vector3(0, 0, -2));
-//
-//    // Create the render target texture
-//    auto renderTexture = RenderTargetTexture::New("render", RenderTargetSize{1024, 512}, scene);
-//    renderTexture->renderList().emplace_back(sphere.get());
-//    planeMaterial->diffuseTexture = renderTexture;
-//
-//    // Unlit material for acurate rendering of texture on plane.
-//    planeMaterial->disableLighting = true;
-//    planeMaterial->emissiveColor = Color3(1, 1, 1);
-//
-//    scene->customRenderTargets.emplace_back(renderTexture.get());
+  }
+  
+  void updateScene()
+  {
+    // TODO: Hook up update to do something and call it
   }
 };
 
